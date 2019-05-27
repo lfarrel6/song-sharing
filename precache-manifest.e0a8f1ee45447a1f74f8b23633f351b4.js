397177df385a9f727b86417bb32cb6b85ef614e0
Liam Farrelly <liam@Liams-MacBook-Pro.local>
2019-05-26 18:31:28 -0700

    Updates

---
+++ precache-manifest.e0a8f1ee45447a1f74f8b23633f351b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b61fd9ea0d5bd00237412d1471007c6b",
+    "url": "/song-sharing/index.html"
+  },
+  {
+    "revision": "ed7d52efcf22f501d135",
+    "url": "/song-sharing/static/css/main.00027cdf.chunk.css"
+  },
+  {
+    "revision": "0b6a58f0f5e54c60eb19",
+    "url": "/song-sharing/static/js/2.7aa23f62.chunk.js"
+  },
+  {
+    "revision": "ed7d52efcf22f501d135",
+    "url": "/song-sharing/static/js/main.e92edd94.chunk.js"
+  },
+  {
+    "revision": "b88cd78f65c1baf03203",
+    "url": "/song-sharing/static/js/runtime~main.932e7c10.js"
+  }
+]);
